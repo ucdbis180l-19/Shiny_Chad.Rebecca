9b76e5a9cdd3140933744e2d3ad228d0f853ee51
Rebecca Ryan <rabednorz@ucdavis.edu>
2019-05-09 22:02:17 +0000

    Data Entry

--- Shiny/server.R
+++ Shiny/server.R
@@ -8,19 +8,46 @@
 #
 
 library(shiny)
+library(tidyverse)
+library(stringr)
+blast_data <- read_tsv("plant_vs_worm.blastout_v2.1.gz",
+                       col_names=c("query_id",
+                                   "subject_id",
+                                   "pct_ident",
+                                   "len",
+                                   "mis",
+                                   "gaps",
+                                   "qb",
+                                   "qe",
+                                   "sb",
+                                   "se",
+                                   "E",
+                                   "Score"))
 
-# Define server logic required to draw a histogram
+
+# Define server logic required to draw a boxplot
 shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
-  })
   
+  # Expression that generates a boxplot. The expression is
+  # wrapped in a call to renderPlot to indicate that:
+  #
+  #  1) It is "reactive" and therefore should re-execute automatically
+  #     when inputs change
+  #  2) Its output type is a plot
+  
+  output$boxPlot <- renderPlot({
+    
+    # set up the plot
+    pl <- ggplot(data = iris,
+                 #Use aes_string below so that input$trait is interpreted
+                 #correctly.  The other variables need to be quoted
+                 aes_string(x="Sepal.Length",
+                            y=input$Species,
+                            fill="Sepal.Length"
+                 )
+    )
+    
+    # draw the boxplot for the specified trait
+    pl + geom_boxplot()
+  })
 })
