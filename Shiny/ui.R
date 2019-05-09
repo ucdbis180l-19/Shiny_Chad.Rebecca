cd6dbbef8c32f7a839359c2573d0d1579ecc3c1c
Chad Pittman <cwpittman@ucdavis.edu>
2019-05-09 23:09:30 +0000

    fdsa

--- Shiny/ui.R
+++ Shiny/ui.R
@@ -1,12 +1,3 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(tidyverse)
 library(stringr)
@@ -24,25 +15,19 @@ blast_data <- read_tsv("plant_vs_worm.blastout_v2.1.gz",
                                    "E",
                                    "Score"))
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
+  titlePanel("Old Faithful Geyser Data"),
   
-  # Application title
-  titlePanel("Plant Vs Worm BLAST"),
-  
-  # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      sliderInput("pct_ident",
-                  "Percent Identity:",
-                  min = 0,
-                  max = 100,
+      sliderInput("Score",
+                  "Score:",
+                  min = 1,
+                  max = 50,
                   value = 30)
     ),
-    
-    # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("pointPlot")
+      plotOutput("boxPlot")
     )
   )
 ))
