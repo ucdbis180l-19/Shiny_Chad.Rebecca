f0cd4cfb8be6bdd4fb4da676d30ceca404ddb160
Rebecca Ryan <rabednorz@ucdavis.edu>
2019-05-13 20:44:04 +0000

    Added Description of App

--- Shiny/ui.R
+++ Shiny/ui.R
@@ -3,6 +3,7 @@
 shinyUI(fluidPage(
   
   titlePanel("Plant Vs Worm BLAST"),
+  "Using BLAST data that compares the genetic make-up of C.elegans and A.thaliana, we can look at various aspects of that BLAST data and look for trends",
   
   sidebarLayout(
     sidebarPanel(
